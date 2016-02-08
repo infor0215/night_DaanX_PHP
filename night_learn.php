8329f65b705f71b52e21ea193311e5bdbd783875
Lian0123 <lienyungli123@gmail.com>
2016-02-08 13:51:12 +0800

    未測試程式碼 統測完在進行測試

--- night_learn.php
+++ night_learn.php
@@ -4,30 +4,23 @@
 include的程式
 第二階段的程式
 ==========================*/
-	$sum=0;//設定記數初始值
-	$read_data=0;//設定記數初始值
 	$read_SQL=mysql_query("SELECT * FROM night_learn_sgin");//尋找資料表
 	$row_id_01 = mysql_fetch_row($read_SQL);//搜索列
 	$number=$row_id_01[0]+1;//取得第一個列的值（id）即上次搜索到的最終資料+1
 	$array_unmber="";//設定紀錄用的陣列變數
-	for ($i=$number; $i <$number+20 ; $i++) { //將＄i設為上次搜索到的最終資料 並以每二十個資料為一循環
+	$max_number=$number+10;
+	for ($i=$number; $i < $max_number; $i++) { //將＄i設為上次搜索到的最終資料 並以每二十個資料為一循環
 		$hear=get_headers("http://night.taivs.tp.edu.tw/news/u_news_v2.asp?id={A530345D-1603-4664-B4F6-841F061340FA}&newsid=$i");//取得網站標頭（＄i為上次搜尋到的數字累加）
 		$text=implode("", array_intersect(str_split($hear[2]), array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)));//選取網站標頭的大小（$hear[2]） 並取得其大小（KB）
 		if($test>=10800){
 			$array_unmber[]=$i;//將（存在的）資料 存入$array_unmber的陣列中
-			$sum=0;
-		}
-		elseif($test<10800&&$sum<20){
-			$sum=$sum+1;
-		}
-		elseif($sum==20){
-			break;
+			$max_number=$max_number+1;
 		}
 	}
 	if($array_unmber !=""){//當$array_unmber存在時
 		$last_number=max($array_unmber);//將陣列中最大值取出
 		mysql_query(" UPDATE night_learn_sgin SET id='$last_number'") ;//寫回SQL
-		for ($t=0; $t <$last_number ; $t++) { 		
+		for ($t=0; $t <$last_number ; $t++) {
 			$ch=curl_init();//curl宣告(第一階段)
 			curl_setopt($ch, CURLOPT_URL,"http://night.taivs.tp.edu.tw/news/u_news_v2.asp?id={A530345D-1603-4664-B4F6-841F061340FA}&newsid=$array_unmber[$t]");//網頁來源宣告(第一階段)
 			curl_setopt($ch, CURLOPT_HEADER, false);//頁面標籤顯示(第一階段)
